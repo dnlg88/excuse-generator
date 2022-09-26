1dc5988b85278e553c594ff8d46af3fc46bf2c49
Daniel Guzmán <dnlguzman@gmail.com>
2022-09-26 20:33:17 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -8,4 +8,41 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  function mentira() {
+    //write your code here
+    let who = [
+      "The dog",
+      "My grandma",
+      "His turtle",
+      "My bird",
+      "My crazy uncle"
+    ];
+    let action = ["ate", "peed", "broke", "crushed", "lost"];
+    let what = [
+      "my homework",
+      "the car",
+      "the keys",
+      "the contract",
+      "my computer"
+    ];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let text =
+      who[Math.floor(Math.random() * 5)] +
+      " " +
+      action[Math.floor(Math.random() * 5)] +
+      " " +
+      what[Math.floor(Math.random() * 5)] +
+      " " +
+      when[Math.floor(Math.random() * 5)];
+    return text;
+  }
+  document.querySelector("#generate").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = mentira();
+  });
 };
